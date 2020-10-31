1aaa2d94125ccde14ac2a35ca76946e64af8e662
Xavier Arias <xavier1923@gmail.com>
2020-10-31 12:32:54 +0100

    Added project to source control

---
+++ Source/LosslessPhysics.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using HarmonyLib;
+
+[KSPAddon(KSPAddon.Startup.SpaceCentre, true)]
+[DefaultExecutionOrder(100)]
+public class LosslessPhysics : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        var harmony = new Harmony("com.metalbass.LosslessPhysics");
+        harmony.PatchAll();
+    }
+}
+
+
+[HarmonyPatch(typeof(TimeWarp), "fixedDeltaTime", MethodType.Getter)]
+class TimeWarp_fixedDeltaTime_return_50Hz
+{
+    static void Postfix(ref float __result)
+    {
+        __result = 1f / 50f;
+    }
+}
+
+
+[HarmonyPatch(typeof(TimeWarp), "updateRate")]
+class TimeWarp_updateRate_set_Time_fixedDeltaTime_to_50Hz
+{
+    static void Postfix()
+    {
+        Time.fixedDeltaTime = 1f / 50f;
+    }
+}
+
+
+[HarmonyPatch(typeof(Planetarium), "FixedUpdate")]
+class Planetarium_FixedUpdate_uses_TimeScale_One_during_TimeWarp_mode_LOW
+{
+    static void Prefix(out double __state)
+    {
+        __state = Planetarium.TimeScale;
+
+        if (TimeWarp.fetch?.Mode == TimeWarp.Modes.LOW)
+        {
+            Planetarium.TimeScale = 1;
+        }
+    }
+
+    static void Postfix(double __state)
+    {
+        Planetarium.TimeScale = __state;
+    }
+}
